2c8861c0e9104009b1993fe66e66a3240731f137
nau20 <95400353+nau20@users.noreply.github.com>
2023-08-12 11:52:54 +0900

    update logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 import { Navbar, Footer } from "./components";
 import { Home, Resume, About, Contact, Product, Educated } from "./page";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="bg-gradient-to-r from-sky-300 to-green-300 overflow-hidden h-auto">
